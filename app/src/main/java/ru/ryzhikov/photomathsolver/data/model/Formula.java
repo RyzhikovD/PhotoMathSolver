acf7ee0b5b85a78076e9039729ee5eed5fb47351
dmitriy <ryzhikov.dmvl@gmail.com>
2019-11-26 16:52:44 +0300

    Formats added to Mathpix API request

--- app/src/main/java/ru/ryzhikov/photomathsolver/data/model/Formula.java
+++ app/src/main/java/ru/ryzhikov/photomathsolver/data/model/Formula.java
@@ -5,11 +5,99 @@ import com.google.gson.annotations.SerializedName;
 
 public class Formula {
 
-    @SerializedName("latex")
+    @SerializedName("latex_simplified")
     @Expose
     private String mLatex;
 
+//    @SerializedName("detection_map")
+//    @Expose
+    private DetectionMap mDetectionMap;
+
+//    @SerializedName("error")
+//    @Expose
+//    private String mError;
+
+    @SerializedName("detection_list")
+    @Expose
+    private String[] mLatexList;
+
+    @SerializedName("latex_confidence")
+    @Expose
+    private double mLatexConfidence;
+
+    @SerializedName("latex_confidence_rate")
+    @Expose
+    private double mLatexConfidenceRate;
+
+//    @SerializedName("position")
+//    @Expose
+    private Position mPosition;
+
+    @SerializedName("wolfram")
+    @Expose
+    private String mWolfram;
+
+    public static class DetectionMap {
+        @SerializedName("contains_chart")
+        @Expose
+        public double contains_chart;
+
+        @SerializedName("contains_diagram")
+        @Expose
+        public double contains_diagram;
+
+//        @SerializedName("contains_geometry")
+//        @Expose
+//        public double contains_geometry;
+
+        @SerializedName("contains_graph")
+        @Expose
+        public double contains_graph;
+
+        @SerializedName("contains_table")
+        @Expose
+        public double contains_table;
+
+        @SerializedName("is_inverted")
+        @Expose
+        public double is_inverted;
+
+        @SerializedName("is_not_math")
+        @Expose
+        public double is_not_math;
+
+        @SerializedName("is_printed")
+        @Expose
+        public double is_printed;
+
+        @SerializedName("is_blank")
+        @Expose
+        public double is_blank;
+    }
+
+    public static class Position {
+        @SerializedName("width")
+        @Expose
+        public double width;
+
+        @SerializedName("height")
+        @Expose
+        public double height;
+
+        @SerializedName("top_left_x")
+        @Expose
+        public double top_left_x;
+
+        @SerializedName("top_left_y")
+        @Expose
+        public double top_left_y;
+    }
+
     public String getLatex() {
         return mLatex;
+    }
+
+    public String getWolfram() {
+        return mWolfram;
     }
 }
--- app/src/main/java/ru/ryzhikov/photomathsolver/data/model/RequestBody.java
+++ app/src/main/java/ru/ryzhikov/photomathsolver/data/model/RequestBody.java
@@ -2,8 +2,10 @@ package ru.ryzhikov.photomathsolver.data.model;
 
 public class RequestBody {
     final String url;
+    final String[] formats;
 
-    public RequestBody(String url) {
+    public RequestBody(String url, String[] formats) {
         this.url = url;
+        this.formats = formats;
     }
 }
