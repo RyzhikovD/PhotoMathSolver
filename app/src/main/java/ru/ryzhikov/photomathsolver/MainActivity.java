e8eb2405c5da97c3542701470037d332708eceda
dmitriy <ryzhikov.dmvl@gmail.com>
2019-11-30 20:17:53 +0300

    Added ChooseImageFragment

--- app/src/main/java/ru/ryzhikov/photomathsolver/MainActivity.java
+++ app/src/main/java/ru/ryzhikov/photomathsolver/MainActivity.java
@@ -1,34 +1,18 @@
 package ru.ryzhikov.photomathsolver;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import ru.ryzhikov.photomathsolver.fragment.CropPhotoFragment;
-import ru.ryzhikov.photomathsolver.provider.FileProvider;
+import ru.ryzhikov.photomathsolver.fragment.ChooseImageFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int PERMISSION_TO_USE_EXTERNAL_STORAGE = 0;
-
-    private static final int CAMERA_REQUEST = 1;
-
-    private Uri mImageUri;
-
-    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,76 +30,27 @@ public class MainActivity extends AppCompatActivity {
                     ActivityCompat.requestPermissions(this,
                             new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                             PERMISSION_TO_USE_EXTERNAL_STORAGE);
-
                 }
             } else {
-                try {
-                    dispatchTakePictureIntent();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                chooseImage();
             }
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode) {
             case PERMISSION_TO_USE_EXTERNAL_STORAGE: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    try {
-                        dispatchTakePictureIntent();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    chooseImage();
                 }
             }
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.root, CropPhotoFragment.newInstance(mImageUri, mCurrentPhotoPath))
-                    .commit();
-        }
-    }
-
-    private void dispatchTakePictureIntent() throws IOException {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-            if (photoFile != null) {
-                mImageUri = FileProvider.getUriForFile(MainActivity.this,
-                        BuildConfig.APPLICATION_ID + ".provider",
-                        createImageFile());
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
-                startActivityForResult(takePictureIntent, CAMERA_REQUEST);
-            }
-        }
-    }
-
-    private File createImageFile() throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_DCIM), "Camera");
-        File image = File.createTempFile(
-                imageFileName,
-                ".jpg",
-                storageDir
-        );
-
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
+    private void chooseImage() {
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.root, ChooseImageFragment.newInstance())
+                .commit();
     }
 }
